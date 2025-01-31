8a0744c4347143dd614f5ea306bded705225868d
IlliaCoder <153951865+user14923929@users.noreply.github.com>
2025-01-31 22:52:16 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+//% color="#AA278D"
+namespace gesture {
+
+
+    /**
+     * Show icon image on gesture.
+     */
+    //% block
+    export function showIconImageOnGesture(gesture: Gesture, icon: IconNames) {
+        while (true) {
+            if (input.isGesture(gesture)) {
+                basic.showIcon(IconNames.Heart)
+                basic.pause(1000)
+                basic.clearScreen()
+                basic.pause(1000)
+            }
+        }
+    }
+}
---
+++ test.ts
@@ -0,0 +1,1 @@
+gesture.showIconImageOnGesture(Gesture.Shake, IconNames.Heart)
